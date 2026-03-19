35d019393771c4bc8d2b3624d064a36cc4bf358c
Andy <andrewjackp@gmail.com>
2026-03-18 21:30:16 -0700

    add db imports to command
    Signed-off-by: Andy <andrewjackp@gmail.com>

--- app/Console/Commands/DebugArticlesSchema.php
+++ app/Console/Commands/DebugArticlesSchema.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class DebugArticlesSchema extends Command
 {
