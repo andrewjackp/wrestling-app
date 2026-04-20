d46fb776a8d6a53698e23b1b38e234a1a6e13a6c
Andy <andrewjackp@gmail.com>
2026-04-20 07:22:15 -0700

    Add dropdown styling with JS
    Signed-off-by: Andy <andrewjackp@gmail.com>

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
 import './bootstrap';
+
+document.querySelectorAll('.nav-dropdown').forEach(dropdown => {
+  const trigger = dropdown.querySelector('.nav-link');
+
+  trigger.addEventListener('click', (e) => {
+    e.stopPropagation();
+    dropdown.classList.toggle('open');
+  });
+});
+
+// Close when tapping outside
+document.addEventListener('click', () => {
+  document.querySelectorAll('.nav-dropdown.open')
+    .forEach(d => d.classList.remove('open'));
+});
