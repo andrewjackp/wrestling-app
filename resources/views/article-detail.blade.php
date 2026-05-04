545cf8999f2ae89abc4a156340541eb1ff1c13a9
Andy <andrewjackp@gmail.com>
2026-05-04 09:09:02 -0700

    add no-border style
    Signed-off-by: Andy <andrewjackp@gmail.com>

--- resources/views/article-detail.blade.php
+++ resources/views/article-detail.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
   <inner-column>
-    <x-card class='article-card'>
+    <x-card class='article-card no-border'>
       <h1 class="attention-voice">{{ $article->article_title }}</h1>
       <p class="soft-voice">{{ $article->content }}</p>
     </x-card>
